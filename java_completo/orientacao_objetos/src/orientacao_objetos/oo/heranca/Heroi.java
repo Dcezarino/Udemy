1ee03817a9f4761a9aa4f3a25ef9fa3f6ef4bfa4
Diego Cezarino <cezarino.diego@gmail.com>
2023-04-06 21:53:08 -0300

    Exemplo utilizando Herança.

---
+++ java_completo/orientacao_objetos/src/orientacao_objetos/oo/heranca/Heroi.java
@@ -0,0 +1,5 @@
+package orientacao_objetos.oo.heranca;
+
+public class Heroi extends Jogador {
+
+}
---
+++ java_completo/orientacao_objetos/src/orientacao_objetos/oo/heranca/Monstro.java
@@ -0,0 +1,5 @@
+package orientacao_objetos.oo.heranca;
+
+public class Monstro extends Jogador{
+
+}
